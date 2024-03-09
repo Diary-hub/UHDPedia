a7addc463eaebeed7712bb1a01c874b8458daf87
Diary-hub <diary.313205014@uhd.edu.iq>
2024-03-09 23:43:01 +0300

    Final Clean Edit

--- lib/app.dart
+++ lib/app.dart
@@ -1,13 +1,17 @@
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:responsive_sizer/responsive_sizer.dart';
 
 class App extends StatelessWidget {
   const App({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      home: Scaffold(),
-    );
+    return ResponsiveSizer(builder: (context, orientation, screenType) {
+      return const GetMaterialApp(
+        home: Scaffold(),
+      );
+    });
   }
 }
 
